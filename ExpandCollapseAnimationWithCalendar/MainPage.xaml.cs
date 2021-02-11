9a80c67305888d7a8532fb2f30595ce0286e4112
“ShahulAndro” <shahul.446@gmail.com>
2021-02-11 11:17:09 +0800

    adding files

---
+++ ExpandCollapseAnimationWithCalendar/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace ExpandCollapseAnimationWithCalendar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnExpanderTapped(object sender, EventArgs e)
+        {
+            Console.WriteLine("Expander tapped.");
+        }
+
+        void OnLabelTapped(object sender, EventArgs e)
+        {
+            Label label = sender as Label;
+            Expander expander = label.Parent.Parent.Parent as Expander;
+
+            if (label.FontSize == Device.GetNamedSize(NamedSize.Default, label))
+            {
+                label.FontSize = Device.GetNamedSize(NamedSize.Large, label);
+            }
+            else
+            {
+                label.FontSize = Device.GetNamedSize(NamedSize.Default, label);
+            }
+            expander.ForceUpdateSize();
+        }
+    }
+}
